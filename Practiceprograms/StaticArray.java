e77818f101efdd53ab0810575d7f174699d5871c
mailarutharunvinay25csm-glitch <mailarutharunvinay.25.csm@anits.edu.in>
2026-08-07 18:27:45 +0530

    Create StaticArray.java

---
+++ Practiceprograms/StaticArray.java
@@ -0,0 +1,13 @@
+public class StaticArray {
+    public static void main(String[] args) {
+        int x[]={50,60,70,42};
+        for(int i=0;i<x.length;i++){
+            System.out.println(x[i]+ "\t");
+            
+
+
+        }
+
+    }
+    
+}
