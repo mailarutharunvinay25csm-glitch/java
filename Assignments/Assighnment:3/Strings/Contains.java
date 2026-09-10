711d98e278e67a69e21a1ce1b9a04a28ab097532
mailarutharunvinay25csm-glitch <mailarutharunvinay.25.csm@anits.edu.in>
2026-09-10 16:56:17 +0530

    Create Contains.java

---
+++ Assignments/Assighnment:3/Strings/Contains.java
@@ -0,0 +1,14 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+String str = "Tharun vinay is an artist";
+
+System.out.println(str.contains("artist"));
+
+
+
+
+
+    }
+    
+}
