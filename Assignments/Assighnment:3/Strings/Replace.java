6fbeff989a1bc152b9bcd2a4bb48419ff6c0a2fd
mailarutharunvinay25csm-glitch <mailarutharunvinay.25.csm@anits.edu.in>
2026-09-10 16:58:53 +0530

    Create Replace.java

---
+++ Assignments/Assighnment:3/Strings/Replace.java
@@ -0,0 +1,13 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+String str = "Tharun vinay is an artist";
+System.out.println(str.replace("an artist", "a cricket player"));
+
+
+
+
+
+    }
+    
+}
