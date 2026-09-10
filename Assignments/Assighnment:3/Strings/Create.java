47b09146937b858aaeb40924da7ee0570d02867b
mailarutharunvinay25csm-glitch <mailarutharunvinay.25.csm@anits.edu.in>
2026-09-10 16:21:37 +0530

    Create Create.java

---
+++ Assignments/Assighnment:3/Strings/Create.java
@@ -0,0 +1,11 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+        String s = "Hello";
+String s2 = new String("World");
+System.out.println(s);
+System.out.println(s2);
+    }
+    
+}
+
