6ed0d750c36e357599cec16e655ef3ff7c5c99d1
mailarutharunvinay25csm-glitch <mailarutharunvinay.25.csm@anits.edu.in>
2026-09-10 16:35:07 +0530

    Create EqualsIgnoreCase.java

---
+++ Assignments/Assighnment:3/Strings/EqualsIgnoreCase.java
@@ -0,0 +1,10 @@
+
+public class StringsProg {
+    public static void main(String[] args) {
+String s1 = "Tharun";
+String s2 = "ThArUn";
+System.out.println(s1.equalsIgnoreCase(s2));
+
+    }
+    
+}
