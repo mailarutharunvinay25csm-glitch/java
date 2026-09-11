a5b1dd210695408028e6fd7298917acd20fc6096
mailarutharunvinay25csm-glitch <mailarutharunvinay.25.csm@anits.edu.in>
2026-09-11 16:36:21 +0530

    Update Abststraction.java

--- Practiceprograms/Abststraction.java
+++ Practiceprograms/Abststraction.java
@@ -1,5 +1,5 @@
 abstract class Cricket{
-public  abstract void batting();  //abstract method is used when “Every subclass must provide its own implementation of this method.”
+public  abstract void batting();  //abstract method is used when “Every subclass must provide its own implementation of this method.” by overriding
 public void bowling(){
     System.out.println("he is bowller");
 }
